f76faaad687acb30b50ead2a21eb2728aba2a46d
ma7moud332 <119896210+ma7moud332@users.noreply.github.com>
2024-11-21 23:38:53 +0200

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/screens/home_screen.dart';
-import 'package:news_app/services/news_service.dart';
+
 
 void main() {
   
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,6 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/models/article_model.dart';
-import 'package:news_app/services/news_service.dart';
 import 'package:news_app/widgets/categories_list_view.dart';
 import 'package:news_app/widgets/news_list_view_bulider.dart';
-import '../widgets/news_list_view.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
